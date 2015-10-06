2a00f51819d6a5d387587dc232c4c56c420f9e9b
Ridla Fadilah <ridlafadilah@users.noreply.github.com>
2015-10-06 16:10:26 +0700

    tsm tripoin core

---
+++ tripoin-tsm/tsm-web/tripoin-root-core/tripoin-core-model/src/main/java/com/tripoin/core/common/RoleConstant.java
@@ -0,0 +1,13 @@
+package com.tripoin.core.common;
+
+/**
+ * @author <a href="mailto:ridla.fadilah@gmail.com">Ridla Fadilah</a>
+ */
+public class RoleConstant {
+
+	public static final String ROLE_ANONYMOUS = "ROLE_ANONYMOUS";
+	public static final String ROLE_SUPERADMIN = "ROLE_SUPERADMIN";
+	public static final String ROLE_ADMIN = "ROLE_ADMIN";
+	public static final String ROLE_USER = "ROLE_USER";
+
+}
