460cc2a4a18280804db9d10531f9e59732a09b2b
Achmad Fauzi <KNIGHTMASTER@users.noreply.github.com>
2015-11-29 15:14:11 +0700

    +Fauzi ++Recommit

---
+++ tripoin-tsm/tsm-mobile/tripoin-util/src/main/java/com/tripoin/tripoin_util/network/Util.java
@@ -0,0 +1,8 @@
+package com.tripoin.tripoin_util.network;
+
+/**
+ * Created by Achmad Fauzi on 6/4/2015 : 9:59 PM.
+ * @author <a href="fauzi.knightmaster.achmad@gmail.com">Achmad Fauzi</a>
+ */
+public class Util {
+}
