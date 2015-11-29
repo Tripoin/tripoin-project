a655546bcef97c12da7fa90fe344fd8c667e04df
Achmad Fauzi <KNIGHTMASTER@users.noreply.github.com>
2015-11-29 13:05:01 +0700

    +Fauzi --Delete Old Resources

--- tripoin-tsm/tsm-mobile/Tsm-mobile/tripoin-component/src/main/java/com/tripoin/tripoin_component/ui/dialog/Dialog.java
+++
@@ -1,8 +0,0 @@
-package com.tripoin.tripoin_component.ui.dialog;
-
-/**
- * Created by Achmad Fauzi on 6/4/2015 : 10:03 PM.
- * @author <a href="fauzi.knightmaster.achmad@gmail.com">Achmad Fauzi</a>
- */
-public class Dialog {
-}
--- tripoin-tsm/tsm-mobile/Tsm-mobile/tripoin-rest/src/main/java/com/tripoin/tripoin_rest/Rest.java
+++
@@ -1,8 +0,0 @@
-package com.tripoin.tripoin_rest;
-
-/**
- * Created by Achmad Fauzi on 6/4/2015 : 9:59 PM.
- * @author <a href="fauzi.knightmaster.achmad@gmail.com">Achmad Fauzi</a>
- */
-public class Rest {
-}
