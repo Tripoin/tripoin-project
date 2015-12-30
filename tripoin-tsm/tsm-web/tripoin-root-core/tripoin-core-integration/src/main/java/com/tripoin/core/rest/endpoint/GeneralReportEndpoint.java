34027491f4765a7e7f66fb4c551ccea100433e21
Ridla Fadilah <ridlafadilah@users.noreply.github.com>
2015-12-30 21:58:31 +0700

    reporting from datasource grid, update commons-beanutils version, jasperreport version, dtd font families

--- tripoin-tsm/tsm-web/tripoin-root-core/tripoin-core-integration/src/main/java/com/tripoin/core/rest/endpoint/GeneralReportEndpoint.java
+++ tripoin-tsm/tsm-web/tripoin-root-core/tripoin-core-integration/src/main/java/com/tripoin/core/rest/endpoint/GeneralReportEndpoint.java
@@ -1,8 +1,11 @@
 package com.tripoin.core.rest.endpoint;
 
 import java.io.ByteArrayOutputStream;
+import java.sql.Connection;
 import java.util.HashMap;
 import java.util.Map;
+
+import javax.sql.DataSource;
 
 import net.sf.jasperreports.engine.JRDataSource;
 import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
@@ -10,10 +13,15 @@ import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.support.GenericMessage;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.TransactionCallback;
+import org.springframework.transaction.support.TransactionTemplate;
 
 import com.tripoin.core.common.ParameterConstant;
 import com.tripoin.core.common.RoleConstant;
@@ -31,29 +39,51 @@ public class GeneralReportEndpoint extends XReturnStatus {
     @Autowired
     private ReportGenerator reportGenerator;
 
+    @Autowired
+	@Qualifier(value="dataSourceReport")
+    private DataSource dataSourceReport;
+
+    @Autowired
+	@Qualifier(value="transactionManagerReport")
+	private PlatformTransactionManager transactionManagerReport ;
+	
+	private ByteArrayOutputStream outputStreamData;
+	private GeneralReportTransferObject generalReportTransferObject;
+	private Map<String, Object> params;
+	
 	@Secured({RoleConstant.ROLE_SALESMANAGER, RoleConstant.ROLE_ADMIN})
 	public Message<byte[]> exportStreamReport(Message<GeneralReportTransferObject> inMessage){
-		ByteArrayOutputStream outputStreamData = new ByteArrayOutputStream();
-		GeneralReportTransferObject generalReportTransferObject = new GeneralReportTransferObject();
-		Map<String, Object> responseHeaderMap = new HashMap<String, Object>();
-		
+		outputStreamData = new ByteArrayOutputStream();
+		generalReportTransferObject = new GeneralReportTransferObject();
+		Map<String, Object> responseHeaderMap = new HashMap<String, Object>();		
 		try{
 			if(inMessage.getPayload() != null){
 				generalReportTransferObject = inMessage.getPayload();
-				Map<String, Object> params = generalReportTransferObject.getDataFilter();
-				JRDataSource dataSource = null;
-				if(params != null){
-					// TODO
-					dataSource = new JRBeanCollectionDataSource(generalReportTransferObject.getDataSelection());		    		
-		    		params.put("dataSourceManager", dataSource);
-				}else{
-	    			params = new HashMap<String, Object>();
-			    	if(!generalReportTransferObject.getDataSelection().isEmpty()){
-			    		dataSource = new JRBeanCollectionDataSource(generalReportTransferObject.getDataSelection());		    		
-			    		params.put("beanCollectionDataSource", dataSource);
-			    	}	
-				}
-				reportGenerator.printReportToFile(generalReportTransferObject.getTemplateReportName(), dataSource, params, outputStreamData);
+				params = generalReportTransferObject.getDataFilter();
+				if(params == null) params = new HashMap<String, Object>();
+				final TransactionTemplate transactionTemplate = new TransactionTemplate(transactionManagerReport);
+				transactionTemplate.execute(new TransactionCallback<Object>() {
+					@Override
+					public Object doInTransaction(TransactionStatus arg0) {
+						try {
+							if(generalReportTransferObject.getDataSelection() != null && !generalReportTransferObject.getDataSelection().isEmpty()){
+								JRDataSource dataSource = new JRBeanCollectionDataSource(generalReportTransferObject.getDataSelection());		    		
+					    		params.put("beanCollectionDataSource", dataSource);
+					    		reportGenerator.printReportToFile(generalReportTransferObject.getTemplateReportName(), dataSource, params, outputStreamData);
+					    	}else{
+								Connection connection = dataSourceReport.getConnection();		    		
+					    		params.put("dataSourceConnecion", connection);
+					    		reportGenerator.printReportToFile(generalReportTransferObject.getTemplateReportName(), connection, params, outputStreamData);
+							}
+						} catch (Exception e) {
+							LOGGER.error("Generate Report System Error : "+e.getLocalizedMessage(), e);
+							generalReportTransferObject.setResponseCode("1");
+							generalReportTransferObject.setResponseMsg(ParameterConstant.RESPONSE_FAILURE);
+							generalReportTransferObject.setResponseDesc("Generate Report System Error : "+e.getLocalizedMessage());
+						}
+						return null;
+					}
+				});	
 			}
 			generalReportTransferObject.setResponseCode("0");
 			generalReportTransferObject.setResponseMsg(ParameterConstant.RESPONSE_SUCCESS);
