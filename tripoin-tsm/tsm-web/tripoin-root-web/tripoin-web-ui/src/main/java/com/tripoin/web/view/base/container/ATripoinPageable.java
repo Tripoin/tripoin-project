41fd0b7146c8cd66d5a0e37d7b660a1905ff7073
Ridla Fadilah <ridlafadilah@users.noreply.github.com>
2016-02-20 23:32:40 +0700

    core paging maxresult

--- tripoin-tsm/tsm-web/tripoin-root-web/tripoin-web-ui/src/main/java/com/tripoin/web/view/base/container/ATripoinPageable.java
+++ tripoin-tsm/tsm-web/tripoin-root-web/tripoin-web-ui/src/main/java/com/tripoin/web/view/base/container/ATripoinPageable.java
@@ -30,37 +30,6 @@ public abstract class ATripoinPageable<T> {
 	protected void initPaging() {
 		this.getMenuBar().removeItems();
 		if (generalPagingTransferObject.getTotalPage() > 1) {
-			Command eventPageClick = new Command() {
-				private static final long serialVersionUID = 2102926468458330518L;
-				@Override
-				public void menuSelected(MenuItem selectedItem) {
-					selectedItem.setCheckable(true);
-					selectedItem.setChecked(true);
-					if ("".equals(selectedItem.getText()) || selectedItem.getText().isEmpty() || selectedItem.getText() == null) {
-						if (FontAwesome.BACKWARD == selectedItem.getIcon())
-							generalPagingTransferObject.setPositionPage(1);
-						else if (FontAwesome.CARET_LEFT == selectedItem.getIcon()) {
-							generalPagingTransferObject.setPositionPage(generalPagingTransferObject.getPositionPage()-1);
-							if (generalPagingTransferObject.getPositionPage() < 1)
-								generalPagingTransferObject.setPositionPage(1);
-						} else if (FontAwesome.CARET_RIGHT == selectedItem.getIcon()) {
-							generalPagingTransferObject.setPositionPage(generalPagingTransferObject.getPositionPage()+1);
-							if (generalPagingTransferObject.getPositionPage() > generalPagingTransferObject.getTotalPage())
-								generalPagingTransferObject.setPositionPage(generalPagingTransferObject.getTotalPage());
-						} else
-							generalPagingTransferObject.setPositionPage(generalPagingTransferObject.getTotalPage());
-					} else {
-						try {
-							generalPagingTransferObject.setPositionPage(Integer.parseInt(selectedItem.getText()));
-						} catch (Exception e) {
-							generalPagingTransferObject.setPositionPage(1);
-						}
-					}
-					if (generalPagingTransferObject.getPositionPage() > 0) {
-						refreshPageable();
-					}
-				}
-			};
 			Integer posMin = generalPagingTransferObject.getPositionPage() - (new Double(EWebUIConstant.BUTTON_PAGING.getOperatorInt()/2).intValue());
 			if (posMin < 1) posMin = 1;
 			Integer posMax = generalPagingTransferObject.getPositionPage() + (new Double(EWebUIConstant.BUTTON_PAGING.getOperatorInt()/2).intValue());
@@ -68,23 +37,57 @@ public abstract class ATripoinPageable<T> {
 				posMax = generalPagingTransferObject.getTotalPage();
 			if (generalPagingTransferObject.getPositionPage() > 1) {
 				if (posMin > 1)
-					this.getMenuBar().addItem("", FontAwesome.BACKWARD,eventPageClick);
-				this.getMenuBar().addItem("", FontAwesome.CARET_LEFT,eventPageClick);
+					this.getMenuBar().addItem("", FontAwesome.BACKWARD,eventPageClick());
+				this.getMenuBar().addItem("", FontAwesome.CARET_LEFT,eventPageClick());
 			}
 			for (Integer i = posMin; i <= posMax; i++) {
 				if (i == generalPagingTransferObject.getPositionPage()) {
-					MenuItem currentPageItem = this.getMenuBar().addItem(i.toString(), eventPageClick);
+					MenuItem currentPageItem = this.getMenuBar().addItem(i.toString(), eventPageClick());
 					currentPageItem.setCheckable(true);
 					currentPageItem.setChecked(true);
 				} else
-					this.getMenuBar().addItem(i.toString(), eventPageClick);
+					this.getMenuBar().addItem(i.toString(), eventPageClick());
 			}
 			if (generalPagingTransferObject.getPositionPage() < generalPagingTransferObject.getTotalPage()) {
-				this.getMenuBar().addItem("", FontAwesome.CARET_RIGHT,eventPageClick);
+				this.getMenuBar().addItem("", FontAwesome.CARET_RIGHT,eventPageClick());
 				if (posMax < generalPagingTransferObject.getTotalPage())
-					this.getMenuBar().addItem("", FontAwesome.FORWARD,eventPageClick);
+					this.getMenuBar().addItem("", FontAwesome.FORWARD,eventPageClick());
 			}
 		}
+	}
+	
+	protected Command eventPageClick(){
+		return new Command() {
+			private static final long serialVersionUID = 2102926468458330518L;
+			@Override
+			public void menuSelected(MenuItem selectedItem) {
+				selectedItem.setCheckable(true);
+				selectedItem.setChecked(true);
+				if ("".equals(selectedItem.getText()) || selectedItem.getText().isEmpty() || selectedItem.getText() == null) {
+					if (FontAwesome.BACKWARD == selectedItem.getIcon())
+						generalPagingTransferObject.setPositionPage(1);
+					else if (FontAwesome.CARET_LEFT == selectedItem.getIcon()) {
+						generalPagingTransferObject.setPositionPage(generalPagingTransferObject.getPositionPage()-1);
+						if (generalPagingTransferObject.getPositionPage() < 1)
+							generalPagingTransferObject.setPositionPage(1);
+					} else if (FontAwesome.CARET_RIGHT == selectedItem.getIcon()) {
+						generalPagingTransferObject.setPositionPage(generalPagingTransferObject.getPositionPage()+1);
+						if (generalPagingTransferObject.getPositionPage() > generalPagingTransferObject.getTotalPage())
+							generalPagingTransferObject.setPositionPage(generalPagingTransferObject.getTotalPage());
+					} else
+						generalPagingTransferObject.setPositionPage(generalPagingTransferObject.getTotalPage());
+				} else {
+					try {
+						generalPagingTransferObject.setPositionPage(Integer.parseInt(selectedItem.getText()));
+					} catch (Exception e) {
+						generalPagingTransferObject.setPositionPage(1);
+					}
+				}
+				if (generalPagingTransferObject.getPositionPage() > 0) {
+					refreshPageable();
+				}
+			}
+		};
 	}
 
 	public abstract void refreshPageable();
