c67a73ceaff4670425f6c9dc624e860547eeab92
Ridla Fadilah <ridlafadilah@users.noreply.github.com>
2016-02-21 17:00:57 +0700

    core paging maxresult

--- tripoin-tsm/tsm-web/tripoin-root-core/tripoin-core-integration/src/main/java/com/tripoin/core/rest/endpoint/area/AreaLoadEndpoint.java
+++ tripoin-tsm/tsm-web/tripoin-root-core/tripoin-core-integration/src/main/java/com/tripoin/core/rest/endpoint/area/AreaLoadEndpoint.java
@@ -18,36 +18,23 @@ import com.tripoin.core.common.ParameterConstant;
 import com.tripoin.core.common.RoleConstant;
 import com.tripoin.core.dao.filter.ECommonOperator;
 import com.tripoin.core.dao.filter.FilterArgument;
-import com.tripoin.core.dao.filter.PageArgument;
 import com.tripoin.core.dto.AreaData;
 import com.tripoin.core.dto.AreaTransferObject;
 import com.tripoin.core.dto.AreaTransferObject.EnumFieldArea;
 import com.tripoin.core.pojo.Area;
-import com.tripoin.core.pojo.VersionControlSystemTable;
-import com.tripoin.core.rest.endpoint.XReturnStatus;
+import com.tripoin.core.rest.endpoint.base.APageableEndpoint;
 import com.tripoin.core.service.IGenericManagerJpa;
-import com.tripoin.core.service.util.IVersionControlSystemTableService;
 
 /**
  * @author <a href="mailto:ridla.fadilah@gmail.com">Ridla Fadilah</a>
  */
 @Component("areaLoadEndpoint")
-public class AreaLoadEndpoint extends XReturnStatus {
+public class AreaLoadEndpoint extends APageableEndpoint<AreaData> {
 
     private static Logger LOGGER = LoggerFactory.getLogger(AreaLoadEndpoint.class);
 
 	@Autowired
 	private IGenericManagerJpa iGenericManagerJpa;
-	
-	@Autowired
-	private IVersionControlSystemTableService iVersionControlSystemTableService;
-	
-	private VersionControlSystemTable versionControlSystemTable;
-	private Integer positionPage;
-	private Integer rowPerPage;
-	private Integer totalPage;
-	private Integer minRow;
-	private Integer maxRow;
 
 	@Secured({RoleConstant.ROLE_NATIONALSALESMANAGER, RoleConstant.ROLE_ADMIN})
 	public Message<AreaTransferObject> loadArea(Message<AreaData> inMessage){	
@@ -115,45 +102,8 @@ public class AreaLoadEndpoint extends XReturnStatus {
 	public Message<AreaTransferObject> loadAreaPaging(Message<AreaTransferObject> inMessage){	
 		AreaTransferObject areaTransferObject = new AreaTransferObject();
 		Map<String, Object> responseHeaderMap = new HashMap<String, Object>();		
-		try{			
-			Object[] values = null;
-			FilterArgument[] filterArguments = null;
-			if(inMessage.getPayload() != null){
-				areaTransferObject = inMessage.getPayload();				
-				try {					
-					if(areaTransferObject.getFindAreaData() != null){
-						filterArguments = new FilterArgument[]{
-							new FilterArgument(EnumFieldArea.NAME_AREA.toString(), ECommonOperator.LIKE_BOTH_SIDE)
-						};
-						values = new Object[]{areaTransferObject.getFindAreaData().get(EnumFieldArea.NAME_AREA.toString())};
-						versionControlSystemTable = new VersionControlSystemTable();
-						
-						versionControlSystemTable.setTotalRow(((BigInteger)iGenericManagerJpa.getObjectSQLNative("SELECT COUNT(area_id) FROM mst_area WHERE area_name LIKE :".concat(EnumFieldArea.NAME_AREA.toString()), filterArguments, values)).longValue());						
-					}else versionControlSystemTable = iVersionControlSystemTableService.loadValue(Area.TABLE_NAME);						
-					positionPage = areaTransferObject.getPositionPage();
-					rowPerPage = areaTransferObject.getRowPerPage();		
-					if(rowPerPage == null || rowPerPage == 0) rowPerPage = ParameterConstant.ROW_PER_PAGE;
-					totalPage = new Double(versionControlSystemTable.getTotalRow()/rowPerPage).intValue();
-				} catch (Exception e) {
-					LOGGER.error("Load Paging Area System Error : "+e.getLocalizedMessage(), e);
-					rowPerPage = ParameterConstant.ROW_PER_PAGE;
-					versionControlSystemTable = new VersionControlSystemTable();
-					versionControlSystemTable.setTotalRow(new Long(rowPerPage));
-					totalPage = 0;
-				}
-				if(positionPage == null || positionPage <= 0) positionPage = 1;
-				if(versionControlSystemTable.getTotalRow()%rowPerPage>0)totalPage++;	
-				if(positionPage > totalPage) positionPage = totalPage;				
-		        minRow = versionControlSystemTable.getTotalRow().intValue() - (positionPage * rowPerPage);
-		        maxRow = minRow + rowPerPage;
-		        areaTransferObject.setPositionPage(positionPage);
-		        areaTransferObject.setRowPerPage(rowPerPage);
-		        areaTransferObject.setTotalPage(totalPage);
-		        System.out.println("version = "+versionControlSystemTable);
-		        
-			}
-			List<Area> areaList = iGenericManagerJpa.loadObjectsFilterArgument(Area.class, filterArguments, values, null, new PageArgument(minRow, maxRow));
-			System.out.println("arealist = "+areaList);
+		try{
+			List<Area> areaList = iGenericManagerJpa.loadObjectsFilterArgument(Area.class, getFilterArguments(), getValues(), null, getPageTransferObject(inMessage.getPayload(), inMessage.getPayload().getFindAreaData()));
 			List<AreaData> areaDatas = new ArrayList<AreaData>();
 			if(areaList != null){
 				for(int i=areaList.size()-1; i>=0; i--)
@@ -162,6 +112,9 @@ public class AreaLoadEndpoint extends XReturnStatus {
 				areaList = null;
 				areaDatas = null;
 			}
+			areaTransferObject.setPositionPage(getPositionPage());
+			areaTransferObject.setRowPerPage(getRowPerPage());
+			areaTransferObject.setTotalPage(getTotalPage());
 			areaTransferObject.setResponseCode("0");
 			areaTransferObject.setResponseMsg(ParameterConstant.RESPONSE_SUCCESS);
 			areaTransferObject.setResponseDesc("Load Paging Area Data Success");			
@@ -174,8 +127,17 @@ public class AreaLoadEndpoint extends XReturnStatus {
 		setReturnStatusAndMessage(areaTransferObject, responseHeaderMap);
 		Message<AreaTransferObject> message = new GenericMessage<AreaTransferObject>(areaTransferObject, responseHeaderMap);
 		areaTransferObject = null;
-		versionControlSystemTable = null;
 		return message;		
+	}
+
+	@Override
+	protected Long getTotalRowVcsTable() throws Exception {
+		return ((BigInteger)iGenericManagerJpa.getObjectSQLNative("SELECT COUNT(area_id) FROM mst_area WHERE area_name LIKE :".concat(EnumFieldArea.NAME_AREA.toString()), getFilterArguments(), getValues())).longValue();
+	}
+
+	@Override
+	protected String getTableName() {
+		return Area.TABLE_NAME;
 	}
 	
 }
