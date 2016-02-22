f8081f9fdfe68c3c02561d898d4f023be326f4bf
Ridla Fadilah <ridlafadilah@users.noreply.github.com>
2016-02-22 22:47:20 +0700

    refactor

--- tripoin-tsm/tsm-mobile/tripoin-component/src/main/java/com/tripoin/tripoin_component/ui/activity/ICustomActivity.java
+++
@@ -1,9 +0,0 @@
-package com.tripoin.tripoin_component.ui.activity;
-
-/**
- * Created by Achmad Fauzi on 11/19/2014.
- * mailto : fauzi.knightmaster.achmad@gmail.com
- */
-public interface ICustomActivity extends IActivity {
-
-}
--- tripoin-tsm/tsm-mobile/tripoin-rest/src/main/java/com/tripoin/tripoin_rest/post/IPostLogout.java
+++
@@ -1,9 +0,0 @@
-package com.tripoin.tripoin_rest.post;
-
-/**
- * Created on 6/1/2015 : 4:11 PM.
- * @author <a href="fauzi.knightmaster.achmad@gmail.com">Achmad Fauzi</a>
- * @param <RESPONSE>
- */
-public interface IPostLogout<RESPONSE> extends IGenericPost<RESPONSE>{
-}
