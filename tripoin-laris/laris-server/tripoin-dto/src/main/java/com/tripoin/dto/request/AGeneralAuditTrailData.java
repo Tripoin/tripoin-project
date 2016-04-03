cb89d3fec4f09d5afc3bb97cdf621f2bce569b2c
Ridla Fadilah <ridlafadilah@users.noreply.github.com>
2016-04-03 13:30:39 +0700

    tripoin dto

---
+++ tripoin-laris/laris-server/tripoin-dto/src/main/java/com/tripoin/dto/request/AGeneralAuditTrailData.java
@@ -0,0 +1,229 @@
+package com.tripoin.dto.request;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+import org.parceler.Parcel;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * @author <a href="mailto:ridla.fadilah@gmail.com">Ridla Fadilah</a>
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlRootElement(name = "AGeneralAuditTrailData")
+@Parcel
+public class AGeneralAuditTrailData {
+
+	@XmlElement(name = "Status", namespace = "")
+	@SerializedName("status")
+	Integer status;
+
+	@XmlElement(name = "Remarks", namespace = "")
+	@SerializedName("remarks")
+	String remarks;
+	
+	@XmlElement(name = "CreatedBy", namespace = "")
+	@SerializedName("createdBy")
+    String createdBy;
+	
+	@XmlElement(name = "CreatedIP", namespace = "")
+	@SerializedName("createdIP")
+    String createdIP;
+	
+	@XmlElement(name = "CreatedTime", namespace = "")
+	@SerializedName("createdTime")
+    String createdTime;
+	
+	@XmlElement(name = "CreatedPlatform", namespace = "")
+	@SerializedName("createdPlatform")
+    String createdPlatform;
+	
+	@XmlElement(name = "ModifiedBy", namespace = "")
+	@SerializedName("modifiedBy")
+    String modifiedBy;
+	
+	@XmlElement(name = "ModifiedIP", namespace = "")
+	@SerializedName("modifiedIP")
+    String modifiedIP;
+	
+	@XmlElement(name = "ModifiedTime", namespace = "")
+	@SerializedName("modifiedTime")
+    String modifiedTime;
+	
+	@XmlElement(name = "ModifiedPlatform", namespace = "")
+	@SerializedName("modifiedPlatform")
+    String modifiedPlatform;
+
+	public AGeneralAuditTrailData() {}
+
+	public Integer getStatus() {
+		return status;
+	}
+
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+
+	public String getRemarks() {
+		return remarks;
+	}
+
+	public void setRemarks(String remarks) {
+		this.remarks = remarks;
+	}
+
+	public String getCreatedBy() {
+		return createdBy;
+	}
+
+	public void setCreatedBy(String createdBy) {
+		this.createdBy = createdBy;
+	}
+
+	public String getCreatedIP() {
+		return createdIP;
+	}
+
+	public void setCreatedIP(String createdIP) {
+		this.createdIP = createdIP;
+	}
+
+	public String getCreatedTime() {
+		return createdTime;
+	}
+
+	public void setCreatedTime(String createdTime) {
+		this.createdTime = createdTime;
+	}
+
+	public String getCreatedPlatform() {
+		return createdPlatform;
+	}
+
+	public void setCreatedPlatform(String createdPlatform) {
+		this.createdPlatform = createdPlatform;
+	}
+
+	public String getModifiedBy() {
+		return modifiedBy;
+	}
+
+	public void setModifiedBy(String modifiedBy) {
+		this.modifiedBy = modifiedBy;
+	}
+
+	public String getModifiedIP() {
+		return modifiedIP;
+	}
+
+	public void setModifiedIP(String modifiedIP) {
+		this.modifiedIP = modifiedIP;
+	}
+
+	public String getModifiedTime() {
+		return modifiedTime;
+	}
+
+	public void setModifiedTime(String modifiedTime) {
+		this.modifiedTime = modifiedTime;
+	}
+
+	public String getModifiedPlatform() {
+		return modifiedPlatform;
+	}
+
+	public void setModifiedPlatform(String modifiedPlatform) {
+		this.modifiedPlatform = modifiedPlatform;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((createdBy == null) ? 0 : createdBy.hashCode());
+		result = prime * result + ((createdIP == null) ? 0 : createdIP.hashCode());
+		result = prime * result + ((createdPlatform == null) ? 0 : createdPlatform.hashCode());
+		result = prime * result + ((createdTime == null) ? 0 : createdTime.hashCode());
+		result = prime * result + ((modifiedBy == null) ? 0 : modifiedBy.hashCode());
+		result = prime * result + ((modifiedIP == null) ? 0 : modifiedIP.hashCode());
+		result = prime * result + ((modifiedPlatform == null) ? 0 : modifiedPlatform.hashCode());
+		result = prime * result + ((modifiedTime == null) ? 0 : modifiedTime.hashCode());
+		result = prime * result + ((remarks == null) ? 0 : remarks.hashCode());
+		result = prime * result + ((status == null) ? 0 : status.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		AGeneralAuditTrailData other = (AGeneralAuditTrailData) obj;
+		if (createdBy == null) {
+			if (other.createdBy != null)
+				return false;
+		} else if (!createdBy.equals(other.createdBy))
+			return false;
+		if (createdIP == null) {
+			if (other.createdIP != null)
+				return false;
+		} else if (!createdIP.equals(other.createdIP))
+			return false;
+		if (createdPlatform == null) {
+			if (other.createdPlatform != null)
+				return false;
+		} else if (!createdPlatform.equals(other.createdPlatform))
+			return false;
+		if (createdTime == null) {
+			if (other.createdTime != null)
+				return false;
+		} else if (!createdTime.equals(other.createdTime))
+			return false;
+		if (modifiedBy == null) {
+			if (other.modifiedBy != null)
+				return false;
+		} else if (!modifiedBy.equals(other.modifiedBy))
+			return false;
+		if (modifiedIP == null) {
+			if (other.modifiedIP != null)
+				return false;
+		} else if (!modifiedIP.equals(other.modifiedIP))
+			return false;
+		if (modifiedPlatform == null) {
+			if (other.modifiedPlatform != null)
+				return false;
+		} else if (!modifiedPlatform.equals(other.modifiedPlatform))
+			return false;
+		if (modifiedTime == null) {
+			if (other.modifiedTime != null)
+				return false;
+		} else if (!modifiedTime.equals(other.modifiedTime))
+			return false;
+		if (remarks == null) {
+			if (other.remarks != null)
+				return false;
+		} else if (!remarks.equals(other.remarks))
+			return false;
+		if (status == null) {
+			if (other.status != null)
+				return false;
+		} else if (!status.equals(other.status))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "AGeneralAuditTrailData [status=" + status + ", remarks=" + remarks + ", createdBy=" + createdBy
+				+ ", createdIP=" + createdIP + ", createdTime=" + createdTime + ", createdPlatform=" + createdPlatform
+				+ ", modifiedBy=" + modifiedBy + ", modifiedIP=" + modifiedIP + ", modifiedTime=" + modifiedTime
+				+ ", modifiedPlatform=" + modifiedPlatform + "]";
+	}
+
+}
